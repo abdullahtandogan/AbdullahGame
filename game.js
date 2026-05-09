b84ca55b1de5af038e92dba56a292a9e328ef9c5
abdullahtandogan <abdullah10tandogan@gmail.com>
2026-05-09 21:13:53 +0300

    Initialize game with player movement and rendering

--- game.js
+++ game.js
@@ -1,1 +1,45 @@
 
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let player = {
+  x: 100,
+  y: 100,
+  size: 40,
+  speed: 5
+};
+
+let keys = {};
+
+window.addEventListener("keydown", e => {
+  keys[e.key] = true;
+});
+
+window.addEventListener("keyup", e => {
+  keys[e.key] = false;
+});
+
+function update() {
+  if (keys["w"]) player.y -= player.speed;
+  if (keys["s"]) player.y += player.speed;
+  if (keys["a"]) player.x -= player.speed;
+  if (keys["d"]) player.x += player.speed;
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "green";
+  ctx.fillRect(player.x, player.y, player.size, player.size);
+}
+
+function gameLoop() {
+  update();
+  draw();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
